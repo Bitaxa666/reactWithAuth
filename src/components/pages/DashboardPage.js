5b7b211f0e4b6971d48e912333a2b87a13361afd
Vitaliy <mihaylukvv@gmail.com>
2018-04-23 12:49:25 +0300

    add confirm email function

---
+++ src/components/pages/DashboardPage.js
@@ -0,0 +1,25 @@
+/**
+ * Created by user on 4/20/18.
+ */
+import React from 'react';
+import { connect } from 'react-redux';
+import ConfirmEmailMessage from '../../messages/ConfirmEmailMessage';
+import PropTypes from 'prop-types';
+
+const DashboardPage = ({ isConfirmed}) => (
+
+      <div>
+          { !isConfirmed && <ConfirmEmailMessage />}
+      </div>
+
+);
+DashboardPage.propTypes = {
+  isConfirmed: PropTypes.bool.isRequired
+};
+function mapStateToProps(state) {
+    return {
+        isConfirmed: !!state.user.confirmed
+    }
+    
+}
+export default connect(mapStateToProps)(DashboardPage);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import "semantic-ui-css/semantic.min.css";
 import { createStore, applyMiddleware} from 'redux';
 import { Provider } from 'react-redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './rootReducer';
 import thunk from 'redux-thunk';
+import decode from 'jwt-decode';
 import App from './App';
 import { userLoggedIn } from './actions/auth';
 
@@ -16,14 +17,17 @@ const store = createStore(
 );
 
 if(localStorage.bookwormJWT) {
-    const user = { token: localStorage.bookwormJWT };
+    const payload = decode(localStorage.bookwormJWT)
+    const user = { token: localStorage.bookwormJWT,
+        email: payload.email,
+        confirmed: payload.confirmed };
     store.dispatch(userLoggedIn(user));
 }
 
 ReactDOM.render(
     <BrowserRouter>
         <Provider store={store}>
-            <App />
+            <Route component={App}/>
         </Provider>
     </BrowserRouter>,
   document.getElementById('root')
